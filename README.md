f12413e3759fa5275f11c1c2666a34619a09a418
Rillian Grant <rillian.grant@gmail.com>
2022-09-21 21:44:56 +0100

    Auth Setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # django_crud_poc
 Django website proof of concept
+
+# Accounts
+admin admin
+https://stackoverflow.com/a/14587231/10251041
+TODO logout with post request
+logged_out.html not needed
--- django_crud_poc/settings.py
+++ django_crud_poc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'django_crud_poc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +120,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
